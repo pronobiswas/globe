519cee5f37bfcf08fb6c3fa6065343212b4493b9
PRONOB BISWAS <80696443+pronobiswas@users.noreply.github.com>
2025-10-07 17:50:01 +0600

    update

--- script.js
+++ script.js
@@ -15,16 +15,30 @@ class Cell {
     this.slideX = Math.random() * 10;
     this.slideY = 0;
     this.vx = 0;
+    this.vy = 0;
+    this.ease = 0.1;
+    this.friction = 0.8;
   }
 
   draw(context) {
     context.fillStyle = 'blue';
-    context.strokeRect(this.x, this.y, this.width, this.height);
+    // context.strokeRect(this.x, this.y, this.width, this.height);
     context.drawImage(this.image, this.x + this.slideX, this.y + this.slideY, this.width, this.height, this.x, this.y, this.width, this.height);
   }
   update(){
     const dx = this.effect.mouse.x - this.x ;
-    this.slideX += dx;
+    const dy = this.effect.mouse.y - this.y ;
+    const distance = Math.hypot(dx, dy);
+    // const distance = Math.sqrt(dx * dx + dy * dy);
+    if (distance < this.effect.mouse.radius) {
+        const angle = Math.atan2(dy, dx);
+        const force = distance / this.effect.mouse.radius;
+        this.vx = force * Math.cos(angle);
+        this.vy = force * Math.sin(angle);
+    }
+
+    this.slideX += (this.vx *= this.friction) - this.slideX * this.ease;
+    this.slideY += (this.vy *= this.friction) - this.slideY * this.ease;
   }
 }
 
@@ -38,15 +52,18 @@ class Effect {
     this.imageGrid = [];
     this.createGrid();
     this.mouse = {
-        x:null,
-        y:null,
+        x:undefined,
+        y:undefined,
         radius: 100,
     };
     this.canvas.addEventListener('mousemove', (e)=>{
         this.mouse.x = e.offsetX;
         this.mouse.y = e.offsetY;
-        console.log(this.mouse.x, this.mouse.y);
         
+    })
+    this.canvas.addEventListener('mouseleave', (e)=>{
+        this.mouse.x = undefined;
+        this.mouse.y = undefined;
     })
   }
 
@@ -60,7 +77,7 @@ class Effect {
 
   render(context) {
     this.imageGrid.forEach((cell, i) => {
-        // cell.update();
+        cell.update();
         if(i <1000){cell.draw(context)}
     });
   }
